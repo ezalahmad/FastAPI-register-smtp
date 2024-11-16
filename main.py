a059ccd9fcc5d3e8e31e575ec07e9a128b8f9540
El pythonhat <ask.pythonhat@gmail.com>
2024-11-16 15:48:18 +0800

    Set-up jinja templates - `{{ url_for(static, path=/css/style.css) }}` if `path` change to `filename` will produce an error

--- main.py
+++ main.py
@@ -2,14 +2,23 @@
  # main.py
 
 from typing import Union
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+# import os
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/about")
+def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
 
