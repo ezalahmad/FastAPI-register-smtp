24d5d0f8ad19bc37fad9e8d62cc24a094dbf142d
El pythonhat <ask.pythonhat@gmail.com>
2024-11-16 12:27:36 +0800

    setup jinja template for `base` and `home` pages with main nav menu links

--- main.py
+++ main.py
@@ -2,15 +2,14 @@
  # main.py
 
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
