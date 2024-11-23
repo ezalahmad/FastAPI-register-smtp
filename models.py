c6a1fc90345f35af10534ff7a31482eb79d37d1c
El Ahmad <ezalahmad@gmail.com>
2024-11-23 23:45:01 +0800

    save changes before switching branches

--- models.py
+++ models.py
@@ -6,25 +6,25 @@ from schema import Roles
 from connection import Base
 
 
-# class UserModel(Base):
-#     __tablename__ = "users"
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     email = Column(String, unique=True, index=True)
-#     username = Column(String, unique=True, index=True)
-#     password = Column(String, unique=True, index=True)
-#     is_active = Column(Boolean, default=False)
-#     role = Column(Enum(Roles), default="user")
-
-
 class UserModel(Base):
     __tablename__ = "users"
-    id: int
-    username: str
-    password: str
-    email: str
-    is_active: bool = False
 
-    class Config:
-        from_attributes = True  # Replace orm_mode
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String, unique=True, index=True)
+    is_active = Column(Boolean, default=False)
+    role = Column(Enum(Roles), default="user")
+
+
+# class UserModel(Base):
+#     __tablename__ = "users"
+#     id: int
+#     username: str
+#     password: str
+#     email: str
+#     is_active: bool = False
+#
+#     class Config:
+#         from_attributes = True  # Replace orm_mode
 
